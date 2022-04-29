05d1985203d0639c3bd3717e00035f82e529a41f
Salvatore Romano <salvatore.romano@univie.ac.at>
2022-04-29 15:16:50 +0200

    modified main.py and added linalg.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import numpy as np
 
-from std_operation import Add
+from std_operation import Add, Substract
+
+from linalg import ScalProd
 
 a=-1
 b=3.14
 
 c=Add(a,b)
 
-print("{}+{}={}".format(a,b,c))
+c= Add(a,b)
+d = Substract(a,b)
+
+print("{}+{}={}\n[{}-{}".format(a,b,c,a,b,d))
+u = np.array((1, 0.5, -3.14))
+v = np.array((-4,0,6.73))
+
+udotv = ScalProd(u,v)
 
